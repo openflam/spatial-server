1efb49ab779404c4a44625302c097940e8784e18
Sagar Bharadwaj <sagarbharadwaj50@gmail.com>
2024-07-10 13:36:01 -0400

    Download points.pcd file and remove sfm reconstruction

--- spatial_server/server/routes/download_map.py
+++ spatial_server/server/routes/download_map.py
@@ -7,30 +7,39 @@ from flask import Blueprint, render_template, send_file
 bp = Blueprint('download_map', __name__, url_prefix='/download_map')
 
 @bp.route('/<map_name>', methods=['GET'])
-def download_map(map_name):
+def download_map(map_name, download_sfm_recnstruction = False):
     directory = os.path.join('data', 'map_data', map_name)
-    hloc_directory = os.path.join(directory, 'hloc_data', 'scaled_sfm_reconstruction')
-    if not os.path.exists(hloc_directory):
-        hloc_directory = os.path.join(directory, 'hloc_data', 'sfm_reconstruction')
-    images_directory = os.path.join(directory, 'images_8')
 
-    # List of filepaths to include in the zip file with their arc names
-    point_cloud_filepaths = [
-        (os.path.join(hloc_directory, 'cameras.bin'), 'point_cloud/cameras.bin'),
-        (os.path.join(hloc_directory, 'images.bin'), 'point_cloud/images.bin'),
-        (os.path.join(hloc_directory, 'points3D.bin'), 'point_cloud/points3D.bin'),
-    ]
+    all_filepaths = []
+    if download_sfm_recnstruction:
+        hloc_directory = os.path.join(directory, 'hloc_data', 'scaled_sfm_reconstruction')
+        if not os.path.exists(hloc_directory):
+            hloc_directory = os.path.join(directory, 'hloc_data', 'sfm_reconstruction')
+        images_directory = os.path.join(directory, 'images_8')
 
-    image_filepaths = [
-        (os.path.join(images_directory, filename), os.path.join('images', filename))
-        for filename in os.listdir(images_directory)
-    ]
+        # List of filepaths to include in the zip file with their arc names
+        point_cloud_filepaths = [
+            (os.path.join(hloc_directory, 'cameras.bin'), 'point_cloud/cameras.bin'),
+            (os.path.join(hloc_directory, 'images.bin'), 'point_cloud/images.bin'),
+            (os.path.join(hloc_directory, 'points3D.bin'), 'point_cloud/points3D.bin'),
+        ]
+
+        image_filepaths = [
+            (os.path.join(images_directory, filename), os.path.join('images', filename))
+            for filename in os.listdir(images_directory)
+        ]
+
+        all_filepaths = all_filepaths + point_cloud_filepaths + image_filepaths
 
     localizer_url_filepath = [
         (os.path.join(directory, 'localization_url.txt'), 'localization_url.txt')
     ]
 
-    all_filepaths = point_cloud_filepaths + image_filepaths + localizer_url_filepath
+    point_cloud_pcd_filepath = [
+        (os.path.join(directory, 'hloc_data', 'points.pcd'), 'point_cloud.pcd')
+    ]
+
+    all_filepaths = all_filepaths + localizer_url_filepath + point_cloud_pcd_filepath
 
     # Create a zip file of the map data
     # Create a BytesIO object to store the zip file in memory
