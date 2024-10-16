ecb4081a0db9ccd120c4e81a901f2e5c0879216c
Sagar Bharadwaj <sagarbharadwaj50@gmail.com>
2024-10-16 11:57:09 -0400

    Added progress bar to polycam upload

--- spatial_server/server/static/scripts/map_upload/zip_upload.js
+++ spatial_server/server/static/scripts/map_upload/zip_upload.js
@@ -1,4 +1,4 @@
-function uploadZip(serverAddress) {
+function uploadZip(serverAddress, progressBar, submitButton) {
     var name = document.getElementById('name').value;
     var zipFile = document.getElementById('zip').files[0];
 
@@ -11,18 +11,31 @@ function uploadZip(serverAddress) {
     formData.append('name', name);
     formData.append('zip', zipFile);
 
-    fetch(serverAddress, {
-        method: 'POST',
-        body: formData,
-    })
-        .then(response => {
-            if (response.ok) {
-                alert('Uplaoded zip file successfully to ' + serverAddress);
-            } else {
-                alert('Failed to upload zip to the server. Server response: ' + response.statusText);
-            }
-        })
-        .catch(error => {
-            alert('Error uploading zip to the server: ' + error.message);
-        });
+    // Make the progress bar visible and set its width to 0% 
+    progressBar.style.visibility = 'visible';
+    progressBar.style.width = '0%';
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', serverAddress, true);
+
+    xhr.upload.onprogress = function (event) {
+        if (event.lengthComputable) {
+            const percentComplete = (event.loaded / event.total) * 100;
+            progressBar.style.width = percentComplete + '%';
+        }
+    };
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            alert('Uplaoded zip file successfully to ' + serverAddress);
+
+            progressBar.style.width = '100%';
+            submitButton.disabled = true;
+        } else {
+            alert('Failed to upload zip to the server. Server response: ' + response.statusText);
+            submitButton.disabled = true;
+        }
+    };
+
+    xhr.send(formData);
 }
